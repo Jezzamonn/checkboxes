2377e618670ec4330c9389f4676c5eeb904086d1
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2021-01-07 00:10:14 -0800

    Got checkboxes appearing :)

---
+++ src/js/checkbox.js
@@ -0,0 +1,55 @@
+const dampMultiple = 0.9;
+
+export class Checkbox {
+
+    constructor({x, y, dx=0, dy=0}) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.children = [];
+
+        this.elem = document.createElement("input");
+        this.elem.type = "checkbox";
+        this.elem.classList.add('checkbox');
+        this.elem.style.top = this.y + 'px';
+        this.elem.style.left = this.x + 'px';
+
+        this.onChecked = () => {};
+        this.onUnChecked = () => {};
+
+        this.elem.onchange = () => {
+            if (this.elem.checked) {
+                this.onChecked(this);
+            }
+            else {
+                this.onUnChecked(this);
+            }
+        }
+    }
+
+    update() {
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.dx *= dampMultiple;
+        this.dy *= dampMultiple;
+
+        this.elem.style.left = this.x + 'px';
+        this.elem.style.top = this.y + 'px';
+    }
+
+    makeChild() {
+        const angle = 2 * Math.PI * Math.random();
+        const speed = 3;
+        const child = new Checkbox({
+            x: this.x,
+            y: this.y,
+            dx: speed * Math.cos(angle),
+            dy: speed * Math.sin(angle),
+        });
+        return child;
+    }
+
+
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,5 @@
 import Controller from './controller.js';
 
-let canvas = document.getElementById('canvas');
-/** @type {!CanvasRenderingContext2D} */
-let context = canvas.getContext('2d');
-
-// Currently assuming square proportions.
-const SIZE = 500;
-
-let scale = 1;
 let lastTime;
 let controller;
 
@@ -15,9 +7,6 @@ function init() {
     lastTime = Date.now();
     controller = new Controller();
 
-    handleResize();
-    // Set up event listeners.
-    window.addEventListener('resize', handleResize);
     // Kick off the update loop
     window.requestAnimationFrame(everyFrame);
 }
@@ -25,7 +14,6 @@ function init() {
 // TODO: Make tweak this to allow frame skipping for slow computers. Maybe.
 function everyFrame() {
     update();
-    render();
     requestAnimationFrame(everyFrame);
 }
 
@@ -34,37 +22,6 @@ function update() {
     let dt = (curTime - lastTime) / 1000;
     controller.update(dt);
     lastTime = curTime;
-}
-
-function render() {
-    // Clear the previous frame
-    context.resetTransform();
-    context.globalAlpha = 1;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Set origin to middle and scale canvas
-    context.translate(canvas.width / 2, canvas.height / 2);
-    context.scale(scale, scale);
-
-    controller.render(context);
-}
-
-function handleResize(evt) {
-    let pixelRatio = window.devicePixelRatio || 1;
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-
-    canvas.width = width * pixelRatio;
-    canvas.height = height * pixelRatio;
-    canvas.style.width = width + 'px';
-    canvas.style.height = height + 'px';
-
-    // Math.max -> no borders (will cut off edges of the thing)
-    // Math.min -> show all (with borders)
-    // There are other options too :)
-    scale = Math.min(canvas.width, canvas.height) / SIZE;
-
-    render();
 }
 
 init();
