34b7ae0005359f5f8c58cf0a40ca20978dd0e0b1
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2021-01-06 23:27:22 -0800

    Get rid of the gif saving stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
 
 const common = {
     module: {
@@ -27,17 +26,6 @@ const client = {
     devtool: 'source-map'
 }
 
-const gifExport = {
-    entry: './src/js/save-frames.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'save-frames.bundle.js'
-    },
-    target: 'node',
-    externals: [nodeExternals()]
-}
-
 module.exports = [
-    Object.assign({}, common, client),
-    Object.assign({}, common, gifExport)
+    Object.assign({}, common, client)
 ];
